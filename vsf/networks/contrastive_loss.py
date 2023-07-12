2285bc0a7069a894a9fa2cba0729a37f3362a49f
Duc-Anh Nguyen <nda.3157@gmail.com>
2023-07-12 11:37:43 +0700

    proper cosine similarity

--- vsf/networks/contrastive_loss.py
+++ vsf/networks/contrastive_loss.py
@@ -6,7 +6,7 @@ import torch.nn.functional as F
 from torch import nn
 
 
-def info_nce_loss(modal1: tr.Tensor, modal2: tr.Tensor, temp: float = 1):
+def info_nce_loss(modal1: tr.Tensor, modal2: tr.Tensor, temp: float = 1, norm2_eps: float = 1e-8):
     """
     InfoNCE loss between 2 modalities
 
@@ -14,12 +14,17 @@ def info_nce_loss(modal1: tr.Tensor, modal2: tr.Tensor, temp: float = 1):
         modal1: features of modal 1, tensor shape [batch, feature]
         modal2: features of modal 2, tensor shape [batch, feature]
         temp: temperature param
+        norm2_eps: epsilon added to norm2 when calculating cosine similarity to avoid division by 0
 
     Returns:
         a pytorch float
     """
     # tensor shape [batch, batch]
     sim = tr.matmul(modal1, modal2.permute([1, 0]))
+    if norm2_eps > 0:
+        length1 = (modal1 ** 2).sum(dim=-1, keepdims=True)
+        length2 = (modal2 ** 2).sum(dim=-1, keepdims=True)
+        sim /= tr.matmul(length1, length2.permute([1, 0]))
     sim = sim / temp
     # create positive idx tensor on the same device as `sim`
     positive_pair_idx = sim.new(np.arange(sim.shape[0])).long()
@@ -42,7 +47,7 @@ class ContrastiveLoss(nn.Module):
 
 
 class CMCLoss(ContrastiveLoss):
-    def __init__(self, main_modal_idx: int = None, temp: float = 1, *args, **kwargs):
+    def __init__(self, main_modal_idx: int = None, temp: float = 1, norm2_eps: float = 1e-8, *args, **kwargs):
         """
         CMC loss for multiple modals
 
@@ -51,10 +56,12 @@ class CMCLoss(ContrastiveLoss):
                 if None, calculate InfoNCE between all possible pairs,
                 if provided, only calculate for pairs containing it
             temp: temperature param
+            norm2_eps: epsilon added to norm2 when calculating cosine similarity to avoid division by 0
         """
         super().__init__(*args, **kwargs)
         self.main_modal_idx = main_modal_idx
         self.temp = temp
+        self.eps = norm2_eps
 
     def forward(self, all_features: tr.Tensor):
         """
@@ -67,57 +74,59 @@ class CMCLoss(ContrastiveLoss):
         assert len(all_features) > 1, 'At least 2 modals are required for contrastive loss'
 
         if len(all_features) == 2:
-            return info_nce_loss(modal1=all_features[0], modal2=all_features[1], temp=self.temp)
+            return info_nce_loss(modal1=all_features[0], modal2=all_features[1], temp=self.temp, norm2_eps=self.eps)
 
         error = 0
         for modal1_idx, modal2_idx in itertools.combinations(range(len(all_features)), 2):
             if (self.main_modal_idx is None) or (self.main_modal_idx in {modal1_idx, modal2_idx}):
-                error += info_nce_loss(all_features[modal1_idx], all_features[modal2_idx], temp=self.temp)
+                error += info_nce_loss(all_features[modal1_idx], all_features[modal2_idx], temp=self.temp,
+                                       norm2_eps=self.eps)
         return error
 
 
 class CocoaLoss(ContrastiveLoss):
-    def __init__(self, temp: float = 1, *args, **kwargs):
+    def __init__(self, temp: float = 1, norm2_eps: float = 1e-8, *args, **kwargs):
         """
         Calculate COCOA loss (with Cross Entropy) from features of multiple modals.
         Source: https://github.com/cruiseresearchgroup/COCOA/blob/main/src/losses.py
 
         Args:
             temp: temperature param
+            norm2_eps: epsilon added to norm2 when calculating cosine similarity to avoid division by 0
         """
         super().__init__(*args, **kwargs)
         self.temp = temp
-        self.max_neg_sim = None
+        self.eps = norm2_eps
 
     def forward(self, all_features: tr.Tensor):
         assert len(all_features) > 1, 'At least 2 modals are required for contrastive loss'
         num_modal, batch_size, n_channel = all_features.shape
+        feature_norm2 = tr.sqrt((all_features ** 2).sum(dim=-1, keepdims=True)) + self.eps
 
         # Positive pairs
         # similarity between all pairs of modals for each batch, shape [batch, modal, modal]
         pos_similarity = tr.matmul(all_features.permute([1, 0, 2]), all_features.permute([1, 2, 0]))
-        pos_similarity = tr.exp(pos_similarity / self.temp)
-        finite_idx = tr.isfinite(pos_similarity)
-        if not finite_idx.all():
-            max_pos_sim = tr.max(pos_similarity[finite_idx]).detach()
-            pos_similarity = tr.clamp(pos_similarity, max=max_pos_sim)
+        pos_similarity /= tr.matmul(feature_norm2.permute([1, 0, 2]), feature_norm2.permute([1, 2, 0]))
+        # remove diagonal (similarity with itself)
         tri_mask = tr.full_like(pos_similarity, fill_value=True, dtype=tr.bool, requires_grad=False)
         tri_mask[:, tr.arange(num_modal), tr.arange(num_modal)] = False
         pos_similarity = pos_similarity[tri_mask].reshape([-1, num_modal, num_modal - 1])
-        # shape [batch, modal, modal] -> [batch]
+
+        pos_similarity = tr.exp(pos_similarity / self.temp)
+        # shape [batch, modal, modal-1] -> [batch]
         pos_similarity = pos_similarity.sum(dim=[1, 2])
 
         # Negative pairs
         # similarity between all pairs of batch indices for each modal, shape [modal, batch, batch]
         neg_similarity = tr.matmul(all_features, all_features.permute([0, 2, 1]))
-        neg_similarity = tr.exp(neg_similarity / self.temp)
-        if self.max_neg_sim is None:
-            self.max_neg_sim = tr.max(neg_similarity[tr.isfinite(neg_similarity)]).detach()
-        neg_similarity = tr.clamp(neg_similarity, max=self.max_neg_sim)
+        neg_similarity /= tr.matmul(feature_norm2, feature_norm2.permute([0, 2, 1]))
+        # remove diagonal (similarity with itself)
         tri_mask = tr.full_like(neg_similarity, fill_value=True, dtype=tr.bool, requires_grad=False)
         tri_mask[:, tr.arange(batch_size), tr.arange(batch_size)] = False
         neg_similarity = neg_similarity[tri_mask].reshape([-1, batch_size, batch_size - 1])
-        # shape [modal, batch, batch] -> [batch]
+
+        neg_similarity = tr.exp(neg_similarity / self.temp)
+        # shape [modal, batch, batch-1] -> [batch]
         neg_similarity = neg_similarity.mean(dim=2).sum(dim=0)
 
         probs = pos_similarity / (pos_similarity + neg_similarity)
